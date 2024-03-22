5957efd31ed011c9c3ee791ea5ec5f0a704ca8bc
Gabriel Mendes <gabriel.26056@gmail.com>
2024-03-22 19:46:50 -0300

    update

---
+++ programacao-web-mobile/agendamentos/README.md
@@ -0,0 +1,6 @@
+# Como clonar o projeto
+* Clone o projeto com `git clone https://github.com/gabrielMendes21/college.git`
+* Acesse a pasta do projeto: `cd programacao-web-mobile/agendamentos`
+* Instale as dependências: `npm install`
+* inicie o servidor: `npm run dev`
+<!-- * Crie um banco MySQL chamado `sistema` -->
--- programacao-web-mobile/agendamentos/db_connection.js
+++ programacao-web-mobile/agendamentos/db_connection.js
@@ -1,26 +1,26 @@
-import { Sequelize } from 'sequelize';
+// import { Sequelize } from 'sequelize';
 
-// Criação de uma instância do Sequelize
-// A instância já recebe as informações de login e o banco que será usado
-const db = new Sequelize("exemplo", "root", "", {
-    host: "localhost",
-    dialect: "mysql"
-});
+// // Criação de uma instância do Sequelize
+// // A instância já recebe as informações de login e o banco que será usado
+// const db = new Sequelize("sistema", "root", "", {
+//     host: "localhost",
+//     dialect: "mysql"
+// });
 
-// Conectando no banco com as informações passadas na instância do Sequelize
-const connect = () => {
-    db.authenticate()
-    .then(() => {
-        console.log("Banco autenticado");
-    })
-    .catch(err => {
-        console.error({
-            msg: "Falha na conexão com banco",
-            err
-        });
-    });
+// // Conectando no banco com as informações passadas na instância do Sequelize
+// const connect = () => {
+//     db.authenticate()
+//     .then(() => {
+//         console.log("Banco autenticado");
+//     })
+//     .catch(err => {
+//         console.error({
+//             msg: "Falha na conexão com banco",
+//             err
+//         });
+//     });
 
-    return db;
-}
+//     return db;
+// }
 
-export default connect;
+// export default connect;
